25dd4a2124ea6a43f96d2d17c5ff1aca0cc44958
sourabhkv <55890376+sourabhkv@users.noreply.github.com>
2022-04-28 15:52:34 +0530

    Update terminal.bat

--- terminal.bat
+++ terminal.bat
@@ -1,6 +1,6 @@
 @echo off 
 echo YouTube-dl GUI v22.271.02 [sourabhkv] https://github.com/sourabhkv/ytdl
 echo date %date% ,time  %time%.
-echo For help type: yt-dlp --help
+echo For help type: yt-dlp_x86 --help
 echo For help type: ffmpeg --help
 echo WARNING : Use Redirect output (-o [loc])
