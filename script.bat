b51b2e1ef0fb58b146d067410ccd93230fedb7b0
sourabhkv <55890376+sourabhkv@users.noreply.github.com>
2022-06-05 15:49:57 +0530

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,6 +1,6 @@
 @echo off
-taskkill /F /IM updater.exe
-del /f updater.exe
-rename updater2.exe updater.exe
+del /f "Frame 1newer.png"
+rename "Frame 1newer2.png" "Frame 1newer.png"
 PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Updated')"
+"Youtube-dl GUI.exe"
 exit /b
