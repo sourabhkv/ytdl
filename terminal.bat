76f0710050f99167cb6421c104087852131a0b57
sourabhkv <55890376+sourabhkv@users.noreply.github.com>
2022-06-08 20:22:43 +0530

    Update terminal.bat

--- terminal.bat
+++ terminal.bat
@@ -1,5 +1,5 @@
 @echo off 
-echo YouTube-dl GUI v22.271.02 [sourabhkv] https://github.com/sourabhkv/ytdl
+echo YouTube-dl GUI v22.0605.14 [sourabhkv] https://github.com/sourabhkv/ytdl
 echo date %date% ,time  %time%.
 echo For help type: yt-dlp_x86 --help
 echo For help type: ffmpeg --help
