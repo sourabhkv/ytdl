3c6781d5fcaedd4eafb57a56ed80b91e9020a391
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-10-03 19:46:00 +0530

    Update and rename builder.bat to builder.ps1

---
+++ builder.ps1
@@ -0,0 +1,11 @@
+pyinstaller --icon=logo.ico -w --hidden-import yt_dlp.compat._legacy "Youtube-dl GUI.py"
+New-Item -Path ".\dist\Youtube-dl GUI\images" -ItemType Directory
+Copy-Item ".\images\*.*" -Destination ".\dist\Youtube-dl GUI\images"
+Copy-Item logo.ico -Destination ".\dist\Youtube-dl GUI"
+Copy-Item yt-dlp_x86.exe -Destination ".\dist\Youtube-dl GUI"
+Copy-Item version.txt -Destination ".\dist\Youtube-dl GUI"
+Copy-Item ffmpeg.exe -Destination ".\dist\Youtube-dl GUI"
+Copy-Item update_starter.bat -Destination ".\dist\Youtube-dl GUI"
+Remove-Item ".\dist\Youtube-dl GUI\websockets-10.3.dist-info"
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
