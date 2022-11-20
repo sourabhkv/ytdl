60b18f09c453a0c4edfcf3fee707dc808c041519
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-11-20 10:31:58 +0530

    Update builder.ps1

--- builder.ps1
+++ builder.ps1
@@ -6,6 +6,7 @@ Copy-Item logo.ico -Destination ".\dist\Youtube-dl GUI"
 Copy-Item yt-dlp_x86.exe -Destination ".\dist\Youtube-dl GUI"
 Copy-Item version.txt -Destination ".\dist\Youtube-dl GUI"
 Copy-Item ffmpeg.exe -Destination ".\dist\Youtube-dl GUI"
+Copy-Item AtomicParsley.exe -Destination ".\dist\Youtube-dl GUI"
 Copy-Item update_starter.bat -Destination ".\dist\Youtube-dl GUI"
 Remove-Item ".\dist\Youtube-dl GUI\websockets-10.3.dist-info"
 read-host “`r`nPress ENTER to continue...”
