c2c8d5a2d80eafa3ae16345d384d4a3e09b6e0bb
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2023-01-02 22:07:42 +0530

    Update builder.ps1

--- builder.ps1
+++ builder.ps1
@@ -1,12 +1,19 @@
+virtualenv venv
+venv\Scripts\activate
+pip install Pillow
+pip install Pyinstaller
+pip install requests
+pip install psutil
+pip install --upgrade yt_dlp
+pip install pytube
 pyinstaller --icon=logo.ico -w --hidden-import yt_dlp.compat._legacy "Youtube-dl GUI.py"
 New-Item -Path ".\dist\Youtube-dl GUI\images" -ItemType Directory
 Copy-Item ".\images\*.*" -Destination ".\dist\Youtube-dl GUI\images"
 Copy-Item logo.ico -Destination ".\dist\Youtube-dl GUI"
-.\yt-dlp_x86.exe -U
 Copy-Item yt-dlp_x86.exe -Destination ".\dist\Youtube-dl GUI"
 Copy-Item version.txt -Destination ".\dist\Youtube-dl GUI"
 Copy-Item ffmpeg.exe -Destination ".\dist\Youtube-dl GUI"
 Copy-Item AtomicParsley.exe -Destination ".\dist\Youtube-dl GUI"
 Copy-Item update_starter.bat -Destination ".\dist\Youtube-dl GUI"
-Remove-Item ".\dist\Youtube-dl GUI\websockets-10.3.dist-info"
+Remove-Item ".\dist\Youtube-dl GUI\websockets-10.4.dist-info"
 read-host “`r`nPress ENTER to continue...”
