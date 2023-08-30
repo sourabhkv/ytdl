a1d9aeb47fe270c8d228be8af6b7ca96b26fe43a
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2023-08-30 22:55:11 +0530

    adding builder

---
+++ builder.ps1
@@ -0,0 +1,20 @@
+virtualenv venv
+venv\Scripts\activate
+pip install Pillow
+pip install Pyinstaller==5.6.2
+pip install requests
+pip install psutil
+pip install --upgrade yt_dlp
+pip install pytube
+.\yt-dlp_X86 -U
+pyinstaller --icon=.\ytdl\images\logo.ico -w --hidden-import yt_dlp.compat._legacy ".\ytdl\__main__.py"
+New-Item -Path ".\dist\Youtube-dl GUI\images" -ItemType Directory
+Copy-Item ".ytdl\images\*.*" -Destination ".\dist\Youtube-dl GUI\images"
+Copy-Item logo.ico -Destination ".\dist\Youtube-dl GUI"
+Copy-Item yt-dlp_x86.exe -Destination ".\dist\Youtube-dl GUI"
+Copy-Item version.txt -Destination ".\dist\Youtube-dl GUI"
+Copy-Item ffmpeg.exe -Destination ".\dist\Youtube-dl GUI"
+Copy-Item AtomicParsley.exe -Destination ".\dist\Youtube-dl GUI"
+Copy-Item update_starter.bat -Destination ".\dist\Youtube-dl GUI"
+Remove-Item ".\dist\Youtube-dl GUI\websockets-10.4.dist-info"
+read-host “`r`nPress ENTER to continue...”
