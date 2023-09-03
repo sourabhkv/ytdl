74c6a5c3a2818b9eb833139349ae6c0c95f4523e
Varunator <123191558+ForeverAggregrate@users.noreply.github.com>
2023-09-03 21:02:04 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,12 +1,10 @@
-mkdir Build_env
-cd Build_env
 virtualenv venv
 source venv/bin/activate
 pip install Pillow
 pip install pyinstaller
 pip install requests
 pip install --upgrade yt_dlp
-pyinstaller --icon=.images/logo.ico -w --hidden-import yt_dlp.compat._legacy "./ytdl/__main__.py"
+pyinstaller --icon=./images/logo.ico -w --hidden-import yt_dlp.compat._legacy --name="Youtube-dl GUI" ./ytdl/__main__.py
 mkdir "./dist/Youtube-dl GUI/images/"
 cp ../images/* "./dist/Youtube-dl GUI/images/"
 cp ../logo.ico "./dist/Youtube-dl GUI/"
